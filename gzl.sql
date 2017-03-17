e47e358475b24a385ce916cb447060043dbd3ba1
onlucky <onluckys@foxmail.com>
2017-03-17 11:17:01 +0800

    Add files via upload

---
+++ gzl.sql
@@ -0,0 +1,16 @@
+/*
+Navicat Oracle Data Transfer
+Oracle Client Version : 12.1.0.2.0
+
+Source Server         : 172.16.1.15_1521_ORCL
+Source Server Version : 110100
+Source Host           : 172.16.1.15:1521
+Source Schema         : GZL
+
+Target Server Type    : ORACLE
+Target Server Version : 110200
+File Encoding         : 65001
+
+Date: 2016-12-08 13:34:57
+*/
+
